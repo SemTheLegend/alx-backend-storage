5bca0d11b29643133294a92e68e0361fcce6e601
SemTheLegend <semmathe@gmail.com>
2023-04-05 15:06:57 +0000

    first two tasks

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- A SQL that ceates a table `users` 
+-- Column names: id, email, name
+
+CREATE TABLE IF NOT EXISTS users (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255));
