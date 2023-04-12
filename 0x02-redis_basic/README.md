2bacd715cffbd1c3a7a81aa4572e5cfcd880f791
SemTheLegend <semmathe@gmail.com>
2023-04-12 20:56:39 +0000

    task 0. Writing strings to Redis

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,1 @@
+This directory has solutions to tasks based on the Redis API
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module contains a `Cashe` class.
+
+Stores an instance of the `Redis client`.
+"""
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """Cache class."""
+
+    def __init__(self):
+        """Instance of Cache class."""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[int, str, float, bytes]) -> str:
+        """Generates a random key."""
+
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+
+        return random_key
